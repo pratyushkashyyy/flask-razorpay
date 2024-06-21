cc4d0a5b84692c593a3445ccc27e7dd1e2359c19
Pratyush Kashyap <pratyushk403@gmail.com>
2024-06-21 21:44:43 +0530

    added index

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-import requests
-from flask import Flask
+from flask import Flask,render_template,redirect,request
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run()
